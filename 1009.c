ecea747b418c87f0eea07bb87ea078e0eb46d4ba
daniluchetti <70670552+daniluchetti@users.noreply.github.com>
2020-09-18 16:30:06 -0300

    Create  1009.c

---
+++ 1009.c
@@ -0,0 +1,14 @@
+// 1009 - Salário com Bônus
+#include <stdio.h>
+
+int main() {
+
+char primeironomedovendedor;
+double A,B;
+
+scanf("%s",&primeironomedovendedor);
+scanf("%lf %lf",&A,&B);
+printf("TOTAL = R$ %.2f\n",(A+B*0.15));
+
+return 0;
+}
