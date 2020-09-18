33621d7c5f4209f530a5627667b66d5d5db2b262
daniluchetti <70670552+daniluchetti@users.noreply.github.com>
2020-09-18 16:12:10 -0300

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,13 @@
+// 1002 Área do Círculo
+
+#include <stdio.h>
+int main() 
+{
+ 
+    double raio,area;
+    scanf("%lf",&raio);
+    area=3.14159*raio*raio;
+    printf("A=%.4lf\n",area);
+
+return 0;
+}
