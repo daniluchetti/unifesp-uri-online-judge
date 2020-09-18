7781f0b30e3dd3fd6efa7948b2b79edf0e2fc49d
daniluchetti <70670552+daniluchetti@users.noreply.github.com>
2020-09-18 16:28:56 -0300

    Create  1008.c

---
+++ 1008.c
@@ -0,0 +1,12 @@
+// 1008 - Salário
+#include <stdio.h>
+int main() {
+int N,H;
+float VH;
+
+scanf("%d %d %f",&N,&H,&VH);
+printf("NUMBER = %d\n",N);
+printf("SALARY = U$ %.2f\n",(H*VH));
+
+return 0;
+}
