eba410e133a1ebd1e1f12386cdd680244529e3a7
Daniela Luchetti <70670552+daniluchetti@users.noreply.github.com>
2020-09-23 23:31:02 -0300

    Create 1072.c

---
+++ 1072.c
@@ -0,0 +1,25 @@
+// 1072 - Intervalo 2
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(){
+ 
+ int qtd, n, in, out, i;
+ 
+ scanf("%d", &qtd);
+ in = 0;
+ out = 0;
+ 
+ for(i=0;i<qtd;i++){
+  scanf("%d",&n);
+  if((n>=10) && (n<=20)){
+   in++;
+  }else{
+   out++;
+  }
+ }
+ 
+ printf("%d in\n", in);
+ printf("%d out\n",out);
+ return 0;
+}
