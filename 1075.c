ec1f6c02972187de139915048d3d3e0c0159c04a
Daniela Luchetti <70670552+daniluchetti@users.noreply.github.com>
2020-09-23 23:54:25 -0300

    Create 1075.c

---
+++ 1075.c
@@ -0,0 +1,14 @@
+// 1075 - Resto 2
+#include <stdio.h>
+int main(){
+int cont, num;
+
+scanf("%d", &num);
+for(cont=1;cont<=10000;cont++){
+if(cont%num==2){
+printf("%d\n", cont);
+}
+}
+
+return 0;
+}
