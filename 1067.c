05971e4be820999ab4e23e033e61c4451c0b8451
Daniela Luchetti <70670552+daniluchetti@users.noreply.github.com>
2020-09-23 23:14:30 -0300

    Create 1067.c

---
+++ 1067.c
@@ -0,0 +1,13 @@
+// 1067 - Números Ímpares
+#include <stdio.h>
+int main() {
+int cont,num;
+    
+scanf("%d",&num);
+if(num%2==0)
+   num=num-1;
+    for(cont=1;cont<=num;cont+=2)
+      printf("%d\n",cont);
+
+return 0;
+}
