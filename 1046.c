e27c9dfd8ea3174543c879a35682a178e9bbb2d7
Daniela Luchetti <70670552+daniluchetti@users.noreply.github.com>
2020-09-23 22:39:01 -0300

    Create  1046.c

---
+++ 1046.c
@@ -0,0 +1,14 @@
+// 1046 - Tempo de Jogo
+#include <stdio.h>
+int main() {
+int HI,HF,TEMPO;
+
+scanf("%d %d",&HI,&HF);
+TEMPO=24-HI+HF;
+if (TEMPO >24)
+printf("O JOGO DUROU %d HORA(S)\n",TEMPO-24);
+else
+printf("O JOGO DUROU %d HORA(S)\n",TEMPO);
+
+return 0;
+}
